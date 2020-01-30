24009d7df9b6d7187e2b74de449c1643f74602a0
unknown <samuel@zercomsystems.com>
2020-01-30 16:25:08 +0100

    added welcome,logout page

--- index.php
+++ index.php
@@ -2,34 +2,42 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>SQL Injection Example</title>
+    <title>SQL Injection</title>
 
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="bootstrap.min.css" >
 
     <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link rel="stylesheet" href="bootstrap-theme.min.css">
 
     <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="bootstrap.min.js"></script>
 
 </head>
 <body>
+    <center>
+        <span style="color: red;">
+        <?php 
+    $output = "";
+    echo $output 
+    ?>
+    </span>
+</center>
     <div class="container" style="margin-top: 50px; width: 850px;">
         <div class="col-md-6 col-md-offset-3">
             <div class="panel panel-primary">
                 <div class="panel-heading">
-                    <h3>Login</h3>
+                    <h3>SQL INJECTION PANEL</h3>
                 </div>
                  
                 <div style="padding: 10px;">
                     <form action="login.php" method="post" autocomplete="off">
                         <label for="email">Email Address</label><br>
-                        <input type="text" name="email" placeholder="you@hereandthere.com" class="form-control">
+                        <input type="text" name="email" placeholder="your username" class="form-control">
                         <br>
                         <br>
                         <label for="password">Password</label><br>
-                        <input type="password" name="password"  class="form-control">
+                        <input type="password" name="password" placeholder="your password" class="form-control">
 
                         <br><br>
                         <input type="submit" value="Login" class="btn btn-danger btn-block">
